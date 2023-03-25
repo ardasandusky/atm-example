6c29694b3b50e9e8d8aec8abff3e34e40549b415
Arda Erdoğan <125192500+ardaizlata389@users.noreply.github.com>
2023-03-25 22:49:53 +0100

    Initial commit.

---
+++ main.py
@@ -0,0 +1,30 @@
+user1acc = {
+    'name': 'Arda Erdoğan',
+    'accNo': '18500',
+    'balance': 4000,
+    'addBalance': 2500
+}
+
+user2acc = {
+    'name': 'Tuba Saridji',
+    'accNo': '9000',
+    'balance': 3000,
+    'addBalance': 160
+}
+
+
+def withdrawal(account, balance):
+    print(f"Hello, {account['name']}")
+
+    if account['balance'] >= balance:
+        print("You can take your money.")
+    else:
+        total = account['balance'] + account['addBalance']
+
+        if total >= balance:
+            addBalanceUsage = input("Do you want to use additional accounts?(y/n)")
+
+            if addBalanceUsage == "e":
+                print("You can take your money.")
+            else:
+                print(f"{account['balance']} balance in your account numbered {account['accNo']}.")
